4b20990886cae3204faa06544fa46a5121439656
JIAYUAN <cheejiayuan@yahoo.com.sg>
2023-12-15 23:00:22 +0800

    huhuhu

--- components/Contact/Contact.tsx
+++ components/Contact/Contact.tsx
@@ -1,4 +1,5 @@
-import {Title, Text, Container, Button, Overlay, SimpleGrid, ThemeIcon, rem, Stack, Box} from '@mantine/core';
+'use client'
+import {Container, SimpleGrid, Title} from '@mantine/core';
 import classes from './Contact.module.css';
 import {ContactIconsList} from "@/components/Contact/ContactList";
 
@@ -7,17 +8,17 @@ export function Contact() {
 
     return (
 
-        <Container className={classes.wrapper} size={'md'}>
+        <Container className={classes.wrapper} size={'xl'}>
             <Title fw={300} py={30}>Contact Us</Title>
-            <div className={classes.inner}>
-            <div className={classes.contacts}>
-                <ContactIconsList/>
-            </div>
-            <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.793795206038!2d103.8540481086434!3d1.2984692617373257!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31da19affc46e93f%3A0x8152f4416f67e002!2sHARTS%20(BUGIS)!5e0!3m2!1sen!2ssg!4v1702651207035!5m2!1sen!2ssg"
-                width="600"  style={{border:0}} loading="lazy"
-                referrerPolicy="no-referrer-when-downgrade"></iframe>
-            </div>
+            <SimpleGrid cols={{base: 1, sm: 2}} spacing="xl">
+                <div className={classes.contacts}>
+                    <ContactIconsList/>
+                </div>
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.793795206038!2d103.8540481086434!3d1.2984692617373257!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31da19affc46e93f%3A0x8152f4416f67e002!2sHARTS%20(BUGIS)!5e0!3m2!1sen!2ssg!4v1702651207035!5m2!1sen!2ssg"
+                    width="100%" height='100%' style={{border: 0, minHeight: 300}} loading="lazy"
+                    referrerPolicy="no-referrer-when-downgrade"></iframe>
+            </SimpleGrid>
         </Container>
     );
 }
